45e275319461625196ccbfd7a375acefdbdbd2b0
yorhadev <rafael.takano.dev@gmail.com>
2025-02-27 21:37:26 -0300

    feat: created base resource and collection

---
+++ app/Http/Resources/API/BaseResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BaseResource extends JsonResource
+{
+    protected $message;
+
+    protected $errors = [];
+
+    public function __construct($resource, $message = '', $errors = [])
+    {
+        parent::__construct($resource);
+
+        $this->message = $message;
+
+        $this->errors = $errors;
+    }
+
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $response = [
+            'data' => parent::toArray($request),
+            'message' => $this->message,
+        ];
+
+        if (!empty($this->errors)) {
+            $response['errors'] = $this->errors;
+        }
+
+        return $response;
+    }
+}
