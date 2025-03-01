c3e4dbbc1a2f793899259df8aada2c02b88d82b0
yorhadev <rafael.takano.dev@gmail.com>
2025-03-01 14:30:26 -0300

    feat: created genre resource and collection

---
+++ app/Http/Resources/API/GenreResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Request;
+
+class GenreResource extends BaseResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
