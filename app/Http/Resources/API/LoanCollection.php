6725b63ca73bf27d63dc51f95be788ef7a7c81fd
yorhadev <rafael.takano.dev@gmail.com>
2025-03-01 15:41:24 -0300

    feat: created loan resource and controller

---
+++ app/Http/Resources/API/LoanCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Request;
+
+class LoanCollection extends BaseCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
